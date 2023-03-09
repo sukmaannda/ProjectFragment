e7380fa25d0251efc5e57ebd4614eb47a7ce851e
sukmaannda <p.sukmaananda@gmail.com>
2023-03-09 08:23:11 +0700

    first commit

---
+++ app/src/main/java/com/example/projectfragment/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.projectfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView (R.layout.activity_register)
+
+        val viewPager = findViewById<ViewPager>(R.id.view_pager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+
+        viewPager.adapter = ViewPagerAdapter(this, supportFragmentManager)
+        tabs.setupWithViewPager(viewPager)
+    }
+
+}
